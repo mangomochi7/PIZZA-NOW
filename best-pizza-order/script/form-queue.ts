3bf14b7d920883e793a918c4fb2c9ac0d82baa60
maryofmerry <yumary6279@gmail.com>
2026-09-12 10:16:04 -0400

    begin form-queue function

---
+++ best-pizza-order/script/form-queue.ts
@@ -0,0 +1,12 @@
+function getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+function formQueue() {
+    
+    rowName = section + rowNumber
+    const buildQueue[]
+    buildQueue.push (getRandomInt(1,))
+}
